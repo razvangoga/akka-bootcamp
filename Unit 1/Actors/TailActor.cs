cf999a1c3a047d424d05b27c6d23651d035a1c6a
razvan goga <razvan.goga@gmail.com>
2015-08-27 23:00:58 +0300

    Lesson 1.6

--- Unit 1/Actors/TailActor.cs
+++ Unit 1/Actors/TailActor.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinTail
+namespace WinTail.Actors
 {
     /// <summary>
     /// Monitors the file at <see cref="_filePath"/> for changes and sends file updates to console.
@@ -63,15 +63,18 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
+        protected override void PreStart()
+        {
             // start watching file for changes
             _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
             _observer.Start();
@@ -84,6 +87,15 @@ namespace WinTail
             // read the initial contents of the file and send it to console as first message
             var text = _fileStreamReader.ReadToEnd();
             Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            base.PostStop();
         }
 
         protected override void OnReceive(object message)
