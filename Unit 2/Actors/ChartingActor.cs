92693c804bff63876c0aea65bacea248f6aafd1a
razvan goga <razvan.goga@gmail.com>
2015-09-05 21:21:18 +0300

    Lesson 2.5

--- Unit 2/Actors/ChartingActor.cs
+++ Unit 2/Actors/ChartingActor.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : ReceiveActor
+    public class ChartingActor : ReceiveActor, IWithUnboundedStash
     {
         /// <summary>
         /// Maximum number of points we will allow in a series
@@ -66,6 +66,8 @@ namespace ChartApp.Actors
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
         private readonly Button _pauseButton;
+
+        public IStash Stash { get; set; }
 
         public ChartingActor(Chart chart, Button pauseButton) : this(chart, new Dictionary<string, Series>(), pauseButton)
         {
@@ -198,11 +200,15 @@ namespace ChartApp.Actors
 
         private void Paused()
         {
+            Receive<AddSeries>(s => this.Stash.Stash());
+            Receive<RemoveSeries>(s => this.Stash.Stash());
             Receive<Metric>(metric => HandleMetricsPaused(metric));
             Receive<TogglePause>(pause =>
             {
                 SetPauseButtonText(false);
                 UnbecomeStacked();
+
+                this.Stash.UnstashAll();
             });
         }
     }
